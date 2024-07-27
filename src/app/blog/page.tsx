41b5e018314c24c7f6a7f6f8b2516da38969d52f
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-07-27 19:10:20 +0200

    Changed favicon

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,18 @@
 import LastPost from '@/components/blog/latest-post'
 import Navbar from '@/components/shared/navbar'
 import React from 'react'
-import LatestPosts from '@/components/shared/latest-posts';
-import Footer from '@/components/shared/footer';
+import LatestPosts from '@/components/shared/latest-posts'
+import Footer from '@/components/shared/footer'
 
 interface Props {}
 
 function Blog({}: Props) {
-    
-
   return (
     <main className='w-full min-h-screen bg-zinc-100'>
-    <Navbar className='bg-zinc-100/60'/>
-    <LastPost/>
-    <LatestPosts />
-    <Footer/>
+      <Navbar className='bg-zinc-100/60' />
+      <LastPost />
+      <LatestPosts />
+      <Footer />
     </main>
   )
 }
