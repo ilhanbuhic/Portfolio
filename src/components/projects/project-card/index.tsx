67724463c9bb3712c4e11c6e6d88e051c2fede13
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-08-14 00:09:48 +0200

    add project.gif check

--- src/components/projects/project-card/index.tsx
+++ src/components/projects/project-card/index.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { Project } from "../../../../sanity/lib/types/project";
+'use client'
+import { Project } from '../../../../sanity/lib/types/project'
 import {
   Card,
   CardContent,
@@ -7,70 +7,70 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
-import { useState } from "react";
-import imageUrlBuilder from "@sanity/image-url";
-import { client } from "../../../../sanity/lib/client";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+} from '@/components/ui/card'
+import Image from 'next/image'
+import { useState } from 'react'
+import imageUrlBuilder from '@sanity/image-url'
+import { client } from '../../../../sanity/lib/client'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 interface Props {
-  project: Project;
+  project: Project
 }
 
-const builder = imageUrlBuilder(client);
+const builder = imageUrlBuilder(client)
 
 function ProjectCard({ project }: Props) {
-  const [isHovering, setIsHovering] = useState(false);
+  const [isHovering, setIsHovering] = useState(false)
   return (
     <Card
       key={project._id}
-      className="bg-zinc-100/50 backdrop-blur-md h-full"
+      className='bg-zinc-100/50 backdrop-blur-md h-full'
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
       <CardHeader>
-        <CardTitle className="text-lg hover:cursor-pointer hover:underline">
-          <Link href={project.url} target="_blank">
+        <CardTitle className='text-lg hover:cursor-pointer hover:underline'>
+          <Link href={project.url} target='_blank'>
             {project.title}
           </Link>
         </CardTitle>
         <CardDescription>{project.company}</CardDescription>
       </CardHeader>
       <CardContent>
-        {isHovering ? (
+        {isHovering && project.gif ? (
           <Image
             src={builder.image(project.gif).url()}
-            className="rounded-md max-w-full"
-            alt="gif"
+            className='rounded-md max-w-full'
+            alt='gif'
             width={200}
             height={200}
             unoptimized={true}
-            layout={"responsive"}
+            layout={'responsive'}
           ></Image>
         ) : (
           <Image
             src={builder.image(project.image).url()}
-            className="rounded-md max-w-full"
-            alt="site image"
+            className='rounded-md max-w-full'
+            alt='site image'
             width={400}
             height={200}
           />
         )}
       </CardContent>
-      <CardFooter className="flex flex-col items-start gap-3">
+      <CardFooter className='flex flex-col items-start gap-3'>
         <CardDescription>{project.description}</CardDescription>
         <Link
           href={`/projects/${project?.slug.current}`}
-          className={cn(buttonVariants({ variant: "outline", size: "sm" }))}
+          className={cn(buttonVariants({ variant: 'outline', size: 'sm' }))}
         >
           Read More
         </Link>
       </CardFooter>
     </Card>
-  );
+  )
 }
 
-export default ProjectCard;
+export default ProjectCard
